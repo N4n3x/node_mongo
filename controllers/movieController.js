41bf8c9834708f2f04a6d23f168cc90288dba835
Christophe COUERBE <christophe.couerbe@viacesi.fr>
2020-06-10 09:38:08 +0200

    ajout barre de recherche

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -6,11 +6,17 @@ Movie = mongoose.model('movie');
 exports.list_all_movies = async (req, res) => {
   // console.log(req);
   // res.render('movies');
-  const { page = 1, limit = 10, sort = "asc" } = req.query;
+  const { page = 1, limit = 10, sort = "asc", keyword = "" } = req.query;
+  let regex;
+  if(keyword != ""){
+    regex = ".*" + keyword + ".*";
+  }else{
+    regex = "";
+  }
   try { 
-    const count = await Movie.countDocuments();
+    const count = await Movie.countDocuments({ 'title' : {$regex: regex} });
 
-    const movies = await Movie.find()
+    const movies = await Movie.find({ 'title' : {$regex: regex} })
       .limit(limit * 1)
       .skip((page - 1) * limit)
       .sort({year: (sort == "desc" ? -1 : 1)})
@@ -23,7 +29,8 @@ exports.list_all_movies = async (req, res) => {
       pages: Math.ceil(count / limit),
       movies: movies,
       url: req.pathname,
-      sort: sort
+      sort: sort,
+      keyword: keyword
     });
   } catch (err) {
     console.error(err.message);
