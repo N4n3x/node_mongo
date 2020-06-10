3e01f4898d1afcf5d76ba126055af378068b4e50
Christophe COUERBE <christophe.couerbe@viacesi.fr>
2020-06-10 10:05:42 +0200

    optimisation du count

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -14,7 +14,7 @@ exports.list_all_movies = async (req, res) => {
     regex = "";
   }
   try { 
-    const count = await Movie.countDocuments({ 'title' : {$regex: regex} });
+    const count = await Movie.estimatedDocumentCount({ 'title' : {$regex: regex} });
 
     const movies = await Movie.find({ 'title' : {$regex: regex} })
       .limit(limit * 1)
@@ -31,33 +31,6 @@ exports.list_all_movies = async (req, res) => {
       url: req.pathname,
       sort: sort,
       keyword: keyword
-    });
-  } catch (err) {
-    console.error(err.message);
-  }
-};
-
-exports.sort_movies = async (req, res) => {
-  console.log(req);
-  // res.render('movies');
-  const { page = 1, limit = 10 } = req.query;
-
-  try {
-
-    const count = await Movie.countDocuments();
-
-    const movies = await Movie.find()
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .sort({title:-1})
-      .exec()
-    ;
-
-    res.render('movies', {
-      title: 'Movies',
-      currentPage: page,
-      pages: Math.ceil(count / limit),
-      movies: movies
     });
   } catch (err) {
     console.error(err.message);
